52afb1cb0586690d5021b7b135032d795195ed49
DESKTOP-RA0OT43\Sam <manuelal180@hotmail.com>
2025-03-19 11:14:40 +0100

    Practica de Hibernate

---
+++ JakartaEE/hibernate-jpa-app/src/main/java/org/sam/hibernateapp/entity/Cliente.java
@@ -0,0 +1,68 @@
+package org.sam.hibernateapp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="Clientes")
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,  generator = "clientes_seq_generator")
+    @SequenceGenerator(name = "clientes_seq_generator", sequenceName = "clientes_seq", allocationSize = 1)
+    private Long id;
+    private String nombre;
+    private String apellido;
+
+    @Column(name="FORMA_PAGO")
+    private String formaPago;
+
+    public Cliente() {
+    }
+
+    public Cliente(Long id, String nombre, String apellido, String formaPago) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.formaPago = formaPago;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFormaPago() {
+        return formaPago;
+    }
+
+    public void setFormaPago(String formaPago) {
+        this.formaPago = formaPago;
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", formaPago='" + formaPago + '\'';
+    }
+}
