eb588dccb62b31c72f8aa2e5fd6547d24ef64840
DESKTOP-RA0OT43\Sam <manuelal180@hotmail.com>
2025-04-01 11:43:48 +0200

    primefaces con jaas BASIC

--- JakartaEE/webapp-jsf3-primefaces-jaas/src/main/java/org/sam/webapp/jsf3/services/impl/ProductoServiceImpl.java
+++ JakartaEE/webapp-jsf3-primefaces-jaas/src/main/java/org/sam/webapp/jsf3/services/impl/ProductoServiceImpl.java
@@ -1,5 +1,8 @@
 package org.sam.webapp.jsf3.services.impl;
 
+import jakarta.annotation.security.DeclareRoles;
+import jakarta.annotation.security.PermitAll;
+import jakarta.annotation.security.RolesAllowed;
 import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
 import org.sam.webapp.jsf3.entities.Categoria;
@@ -12,6 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Stateless
+@DeclareRoles({"USER", "ADMIN"})
 public class ProductoServiceImpl implements ProductoService {
 
     @Inject
@@ -21,35 +25,42 @@ public class ProductoServiceImpl implements ProductoService {
     private CrudRepository<Categoria> categoriaRepository;
 
     @Override
+    @PermitAll
     public List<Producto> listar() {
         return repository.listar();
     }
 
     @Override
+    @RolesAllowed({"USER", "ADMIN"})
     public Optional<Producto> porId(Long id) {
         return Optional.ofNullable(repository.porId(id));
     }
 
     @Override
+    @RolesAllowed({"ADMIN"})
     public void guardar(Producto producto) {
         repository.guardar(producto);
     }
 
     @Override
+    @RolesAllowed({"ADMIN"})
     public void eliminar(Long id) {
         repository.eliminar(id);
     }
 
     @Override
+    @RolesAllowed({"USER", "ADMIN"})
     public List<Categoria> listarCategorias() {
         return categoriaRepository.listar();
     }
 
     @Override
+    @RolesAllowed({"USER", "ADMIN"})
     public Optional<Categoria> porIdCategoria(Long id) {
         return Optional.ofNullable(categoriaRepository.porId(id));
     }
 
+    @RolesAllowed({"USER", "ADMIN"})
     @Override
     public List<Producto> buscarPorNombre(String nombre) {
         return repository.buscarPorNombre(nombre);
