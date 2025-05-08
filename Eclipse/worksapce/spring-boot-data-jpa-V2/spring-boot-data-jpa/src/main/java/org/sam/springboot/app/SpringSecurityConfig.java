2beb2c8da5993c41298225d43d761b5ae2165848
DESKTOP-RA0OT43\Sam <manuelal180@hotmail.com>
2025-05-08 13:42:58 +0200

    mismo proyecto, pero con spring boot 2.1.4, para poder realizar todos los ejercicios del curso....

---
+++ Eclipse/worksapce/spring-boot-data-jpa-V2/spring-boot-data-jpa/src/main/java/org/sam/springboot/app/SpringSecurityConfig.java
@@ -0,0 +1,59 @@
+package org.sam.springboot.app;
+
+import org.sam.springboot.app.auth.handler.LoginSuccessHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableGlobalMethodSecurity(securedEnabled=true, prePostEnabled=true)
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private LoginSuccessHandler successHandler;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/listar").permitAll()
+		/*.antMatchers("/ver/**").hasAnyRole("USER")*/
+		/*.antMatchers("/uploads/**").hasAnyRole("USER")*/
+		/*.antMatchers("/form/**").hasAnyRole("ADMIN")*/
+		/*.antMatchers("/eliminar/**").hasAnyRole("ADMIN")*/
+		/*.antMatchers("/factura/**").hasAnyRole("ADMIN")*/
+		.anyRequest().authenticated()
+		.and()
+		    .formLogin()
+		        .successHandler(successHandler)
+		        .loginPage("/login")
+		    .permitAll()
+		.and()
+		.logout().permitAll()
+		.and()
+		.exceptionHandling().accessDeniedPage("/error_403");
+
+	}
+
+	@Autowired
+	public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception{
+		
+		PasswordEncoder encoder = this.passwordEncoder;
+		UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+		
+		builder.inMemoryAuthentication()
+		.withUser(users.username("admin").password("123456").roles("ADMIN", "USER"))
+		.withUser(users.username("manuel").password("123456").roles("USER"));
+		
+	}
+
+}
