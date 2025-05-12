8c0d961e9727f3afd95dd6fc974574839194a4fb
DESKTOP-RA0OT43\Sam <manuelal180@hotmail.com>
2025-05-12 09:31:38 +0200

    Vista XML de lista de clientes con factura

--- Eclipse/worksapce/spring-boot-data-jpa-V2/spring-boot-data-jpa/src/main/java/org/sam/springboot/app/MvcConfig.java
+++ Eclipse/worksapce/spring-boot-data-jpa-V2/spring-boot-data-jpa/src/main/java/org/sam/springboot/app/MvcConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 //import org.slf4j.LoggerFactory;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -64,5 +65,10 @@ public class MvcConfig implements WebMvcConfigurer {
 		registry.addInterceptor(localeChangeInterceptor());
 	}
 	
-	
+	@Bean
+	public Jaxb2Marshaller jaxb2Marshaller() {
+		Jaxb2Marshaller marshaller =  new Jaxb2Marshaller();
+		marshaller.setClassesToBeBound(new Class[] {org.sam.springboot.app.view.xml.ClienteList.class});
+		return marshaller;
+	}
 }
