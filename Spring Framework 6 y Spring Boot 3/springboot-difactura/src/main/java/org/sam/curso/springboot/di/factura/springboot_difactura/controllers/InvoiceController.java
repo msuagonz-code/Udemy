68010952dc28ee7b899c8e6002a5a179f134db6f
DESKTOP-RA0OT43\Sam <manuelal180@hotmail.com>
2025-07-18 16:29:01 +0200

    Nuevo proyecto de ejemplo, Factura

---
+++ Spring Framework 6 y Spring Boot 3/springboot-difactura/src/main/java/org/sam/curso/springboot/di/factura/springboot_difactura/controllers/InvoiceController.java
@@ -0,0 +1,21 @@
+package org.sam.curso.springboot.di.factura.springboot_difactura.controllers;
+
+import org.sam.curso.springboot.di.factura.springboot_difactura.models.Invoice;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/invoices")
+public class InvoiceController {
+
+    @Autowired
+    private Invoice invoice;
+
+    @GetMapping("/show")
+    public Invoice show(){
+        return invoice;
+    }
+
+}
