351dd4e495e2a7b6856650319b166d709aef43be
DESKTOP-RA0OT43\Sam <manuelal180@hotmail.com>
2025-07-22 12:01:35 +0200

    Interceptores, ejemplo final

--- Spring Framework 6 y Spring Boot 3/springboot-horario/src/main/java/org/sam/curso/springboot/calendar/interceptor/springboot_horario/controllers/AppController.java
+++ Spring Framework 6 y Spring Boot 3/springboot-horario/src/main/java/org/sam/curso/springboot/calendar/interceptor/springboot_horario/controllers/AppController.java
@@ -8,15 +8,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 public class AppController {
 
     @GetMapping("/foo")
-    public ResponseEntity<?> foo(){
+    public ResponseEntity<?> foo(HttpServletRequest request){
         Map<String, Object> data = new HashMap<>();
         data.put("title", "Bienvenido al sistema de atención!");
         data.put("time", new Date());
-
+        data.put("message", request.getAttribute("message"));
         return ResponseEntity.ok(data);
     }
 
