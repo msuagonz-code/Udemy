55146c5595a0aa50a10d4827207811e57c33f5cc
DESKTOP-RA0OT43\Sam <manuelal180@hotmail.com>
2025-08-24 15:53:28 +0200

    Introducción a Json Web Token

--- Spring Framework 6 y Spring Boot 3/springboot-crud/src/main/java/org/sam/curso/springboot/app/springboot_crud/repositories/UserRepository.java
+++ Spring Framework 6 y Spring Boot 3/springboot-crud/src/main/java/org/sam/curso/springboot/app/springboot_crud/repositories/UserRepository.java
@@ -1,10 +1,14 @@
 package org.sam.curso.springboot.app.springboot_crud.repositories;
 
+import java.util.Optional;
+
 import org.sam.curso.springboot.app.springboot_crud.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long>{
     
     boolean existsByUsername(String username);
+
+    Optional<User> findByUsername(String username);
 
 }
