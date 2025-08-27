f828c5591e953f84155c18bda088083ccbe8e84c
DESKTOP-RA0OT43\Sam <manuelal180@hotmail.com>
2025-08-27 14:05:50 +0200

    Proyecto desplegado en AWS y en Tomcat local mediante el war

--- Spring Framework 6 y Spring Boot 3/springboot-crud/src/main/java/org/sam/curso/springboot/app/springboot_crud/SpringbootCrudApplication.java
+++ Spring Framework 6 y Spring Boot 3/springboot-crud/src/main/java/org/sam/curso/springboot/app/springboot_crud/SpringbootCrudApplication.java
@@ -2,9 +2,10 @@ package org.sam.curso.springboot.app.springboot_crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootCrudApplication {
+public class SpringbootCrudApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCrudApplication.class, args);
